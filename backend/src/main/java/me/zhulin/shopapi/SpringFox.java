6b2c41d37b003fdb916321cebd685e68380ca747
siswarnava-sss <47331441+siswarnava-sss@users.noreply.github.com>
2022-11-05 09:29:25 +0530

    Update SpringFox.java

--- backend/src/main/java/me/zhulin/shopapi/SpringFox.java
+++ backend/src/main/java/me/zhulin/shopapi/SpringFox.java
@@ -10,7 +10,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@EnableSwagger2WebMvc
+
 public class SpringFox {
     @Bean
     public Docket api(){
